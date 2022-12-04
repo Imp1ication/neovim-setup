ace13c38035e42a827839aaddd1a1e935c4dd29f
40947075s <40947075s@ntnu.edu.tw>
2022-12-05 01:08:06 +0800

    update alpha

--- lua/theme/alpha.lua
+++ lua/theme/alpha.lua
@@ -69,9 +69,9 @@ db.section.buttons.val = {
     db.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
     db.button("f", "  Find file", ":Telescope find_files <CR>"),
 --    db.button("p", "  Find project", ":Telescope projects <CR>"),
-    db.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
     db.button("t", "  Find text", ":Telescope live_grep <CR>"),
-    db.button("c", "  Configuration", ":e $MYVIMRC<CR>"),
+    db.button("r", "  Recent files", ":Telescope oldfiles <CR>"),
+    db.button("c", "  Colorscheme", ":Telescope colorscheme <CR>"),
     db.button("q", "  Quit Neovim", ":qa<CR>"),
 }
 
